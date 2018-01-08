9a58a8ad06e0d1f97017a6aae215918248eb3fbe
Duy <duypx@lifetimetech.vn>
2018-01-08 13:33:15 +0700

    Add get size cached and clear cache android

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewModule.java
@@ -1,6 +1,7 @@
 package com.dylanvann.fastimage;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
@@ -9,11 +10,14 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.Priority;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.model.GlideUrl;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
+
+import java.io.File;
 
 class FastImageViewModule extends ReactContextBaseJavaModule {
 
@@ -50,5 +54,65 @@ class FastImageViewModule extends ReactContextBaseJavaModule {
                 }
             }
         });
+    }
+
+    @ReactMethod
+    public void getSizeCached(Promise promise) {
+//        File dirs = new File(getReactApplicationContext().getCacheDir(), DiskCache.Factory.DEFAULT_DISK_CACHE_DIR);
+        File dirs = getReactApplicationContext().getCacheDir();
+
+        try {
+            long totalSize = 0;
+            for (File dir : dirs.listFiles()) {
+                totalSize += calculateSize(dir);
+            }
+
+            promise.resolve(String.valueOf(totalSize));
+        } catch (RuntimeException ex) {
+            promise.resolve(0);
+        }
+    }
+
+
+    @ReactMethod
+    public void clearCached() {
+        deleteCache(getReactApplicationContext());
+    }
+
+    private static long calculateSize(File dir) {
+        if (dir == null) return 0;
+        if (!dir.isDirectory()) return dir.length();
+        long result = 0;
+        File[] children = dir.listFiles();
+        if (children != null)
+            for (File child : children)
+                result += calculateSize(child);
+        return result;
+    }
+
+    private void deleteCache(Context context) {
+        try {
+//            File dirs = new File(getReactApplicationContext().getCacheDir(), DiskCache.Factory.DEFAULT_DISK_CACHE_DIR);
+            File dirs = context.getCacheDir();
+            deleteDir(dirs);
+        } catch (Exception e) {
+        }
+    }
+
+    private static boolean deleteDir(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+            return dir.delete();
+        } else if (dir != null && dir.isFile()) {
+            return dir.delete();
+        } else {
+            return false;
+        }
     }
 }
